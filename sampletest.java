a4fc4036615947571ba3362830b12a9ac1c28326
Whedi540 <hassanwaleed540@gmail.com>
2023-12-10 11:55:36 +0500

    done

--- sampletest.java
+++
@@ -1,9 +0,0 @@
-import static org.junit.Assert.assertEquals;
-import org.junit.Test;
-
-public class SampleTest {
-    @Test
-    public void testAdd() {
-        assertEquals(5, Sample.add(2, 3));
-    }
-}
